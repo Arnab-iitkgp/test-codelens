c2ce7d021761ee0a175f230eb40e6fdfe1eb79d7
Arnab-iitkgp <mrarnab7574@gmail.com>
2025-12-28 16:46:15 +0530

    header added

---
+++ src/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const header = () => {
+  return (
+        <> 
+        <h1>Welcome to My App</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>  
+        </>
+  )
+}
+
+export default header
